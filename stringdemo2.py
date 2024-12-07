f43fd2d442cca5fdb12654cad8edb1a6f52eb338
rutvikmat <ruties4354@gmail.com>
2024-12-07 13:07:50 +0530

    python programs

---
+++ stringdemo2.py
@@ -0,0 +1,10 @@
+str1="java"
+str2="PYTHON"
+str3="welcome to Django"
+print(len(str1))
+print(str1.upper())
+print(str2.lower())
+print(str1.endswith("a"))
+print(str2.startswith("P"))
+print(str3.capitalize())
+print(str3.count("e"))
